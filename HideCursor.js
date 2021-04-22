4562d5edd828375a561e92b5901773d1be7a5a5c
stevie8sena <rasyidsholehrosena@outlook.com>
2021-04-22 14:20:40 +0700

    Create HideCursor.js
    fitur HideCursor.js

---
+++ HideCursor.js
@@ -0,0 +1,11 @@
+//=============================================================================
+// HideCursor.js
+//=============================================================================
+
+/*:
+ * @plugindesc Menyembunyikan kursor mouse.
+ * @author Rasyid Sholeh Rosena
+ *
+ */
+
+document.body.style.cursor = 'none';
